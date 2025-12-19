8985e7c66cd85ed16adc548e5263f5e32318636c
saju-79 <saju98622@gmail.com>
2025-12-19 14:18:37 -0800

    all-ok

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById('btn').addEventListener("click" , function(event){
     const div= document.createElement("div");
     div.innerHTML = `
     
-    <div class="card bg-base-100 w-96 shadow-sm">
+    <div class="card bg-base-300 w-96 shadow-lg">
   <figure class="px-10 pt-10">
     <img
       src=${img}
@@ -16,20 +16,13 @@ document.getElementById('btn').addEventListener("click" , function(event){
       class="rounded-xl" />
   </figure>
   <div class="card-body ">
-    <h2 class="card-title text-2xl">Name: ${name}</h2>
-    <h2 class="card-title text-xl">Roll : ${roll}</h2>
+    <h2 class="card-title text-2xl font-bold ">Name: ${name}</h2>
+    <h2 class="card-title text-xl font-bold ">Roll : ${roll}</h2>
      
   </div>
 </div>
     
-    
-    
-    
-    
-    
-    
-    
-    
+ 
     
     `
     cardMake.appendChild(div)
